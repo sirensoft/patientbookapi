5d6c0866e12883df5ab90886174f067c8be83370
tehnn <tehnnn@gmail.com>
2016-05-26 19:50:03 +0700

    up

--- frontend/controllers/PatientController.php
+++ frontend/controllers/PatientController.php
@@ -1,14 +1,26 @@
 <?php
 
 namespace frontend\controllers;
+use yii;
+use yii\web\Response;
 
-class PatientController extends \yii\web\Controller
-{
-    public function actionIndex($cid=NULL)
-    {
-        return $this->render('index',[
-            'cid'=>$cid
+class PatientController extends \yii\web\Controller {
+    
+    protected function jsonHead(){
+        Yii::$app->response->format = Response::FORMAT_JSON;
+    }
+
+    public function actionIndex($cid = NULL) {
+        return $this->render('index', [
+                    'cid' => $cid
         ]);
+    }
+    
+    public function actionJson(){
+        $this->jsonHead();
+        $arr = ['name'=>'tehnn','lname'=>'jad','age'=>'1111'];        
+        return json_encode($arr);
+        
     }
 
 }
