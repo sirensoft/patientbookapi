89349fc13364a25b1d9c20fa3bcfef80c713d427
tehnn <tehnnn@gmail.com>
2016-06-30 23:12:28 +0700

    utehn

--- frontend/views/patient/input-list.php
+++ frontend/views/patient/input-list.php
@@ -10,13 +10,15 @@ $i=0;
 <?php
 
 if($i%2==0){
-    $color = 'pink';
-}  else {
-   $color='lime';  
+    $color='#00ff7f';     
+    $txt_color = '#000000';
+}  else {   
+   $color = '#3399ff';
+   $txt_color = '#ffffff';
 }
 
 ?>
-<div style="background-color: <?=$color?>;padding-top:5px;padding-bottom: 5px;padding-left: 5px;margin-top: 5px">
+<div style="color:<?=$txt_color?>;background-color: <?=$color?>;padding-top:5px;padding-bottom: 5px;padding-left: 5px;margin-top: 5px">
     <b>วันที่:<?=  $value['date_input']?>  <?=$value['time_input']?></b>
     <br>น้ำหนัก:<?=$value['weight']?>กก. ส่วนสูง:<?=$value['height']?>ซม.  
     <br>รอบเอว:<?=$value['waist']?>ซม. ความดัน:<?=$value['bps']?>/<?=$value['bpd']?>
