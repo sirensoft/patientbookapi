d94018daa3eea4a59091dd92651b89254256863d
tehnn <tehnnn@gmail.com>
2016-07-07 08:33:45 +0700

    utehn-new-init

--- frontend/views/chat/list.php
+++ frontend/views/chat/list.php
@@ -19,7 +19,11 @@ $color = "#c6e2ff";
 $color = "#faebd7";
 ?>
 <div style="text-align:left;margin-bottom: 10px;padding: 5px;background-color: <?=$color?>">
-    <span style="color:gray"><?=$value['send_date']?></span>:&nbsp;
+    <?php
+    $send_date=date_create($value['send_date']);
+    $send_date = date_format($send_date,"Y-m-d H:i");
+    ?>
+    <span style="color:gray"><?=$send_date?></span>:&nbsp;
     <b><?=$value['chat_text']?></b>
     <hr>
     <div style="text-align:left;">
