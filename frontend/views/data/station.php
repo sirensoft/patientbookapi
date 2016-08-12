312cc4b509e804a6c004bb29d406fd44e8e210e2
tehnn <tehnnn@gmail.com>
2016-08-12 13:13:56 +0700

    utehn-new-init

--- frontend/views/data/station.php
+++ frontend/views/data/station.php
@@ -8,16 +8,16 @@
 $data = $raw[0];
 ?>
 <div class="content">
-    <h3 class="address">
+    <h1 class="address">
         <?=$data['hospname']?>
         
-    </h3>
+    </h1>
     <h4 class="address">
         <?=$data['address']?>
     </h4>
-    <h4 class="address">
+    <h2 class="address">
         <?=$data['phone']?>
-    </h4>
+    </h2>
     
 </div>
 
